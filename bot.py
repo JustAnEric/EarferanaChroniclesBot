aecd554145a7f09516bef1393a5490e9c51ebe8f
JustAnEric <73326807+JustAnEric@users.noreply.github.com>
2023-12-18 22:00:13 +1100

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,4 +2,27 @@ from discord.ext import commands
 
 class BotApplication(commands.Bot):
   def __init__(self):
-    super().__init__()
+    super().__init__(
+      command_prefix = "rp!",
+      case_insensitive = True, 
+      intents = discord.Intents(
+        guild_messages = True, 
+        guild_reactions = True, 
+        guilds = True, 
+        invites = True, 
+        members = True, 
+        messages = True, 
+        reactions = True, 
+        message_content = True
+      )
+    )
+
+  async def on_ready(self):
+    print("hello fellow abuc nbuc akmc members! i'm online!")
+    await self.change_presence(
+      activity=discord.Activity(
+        type=discord.ActivityType.watching, 
+        name="rp!help"
+      ),
+      status=discord.Status.dnd
+    )
